bef7e20fd7e3d6ef67895e741f4ac00b49828e0e
Kolia561 <nicolas.blanco@est.fi.uncoma.edu.ar>
2020-11-12 23:17:26 -0300

    primeros mensajes

--- src/Museo/Sala.java
+++ src/Museo/Sala.java
@@ -4,27 +4,57 @@ public class Sala {
 
     private int cupo;
     private int temperatura;
+    private int jubilados;
+    private int dentro;
 
     public Sala(int capacidad) {
 
         this.cupo = capacidad;
-        this.temperatura= 25;
+        this.temperatura = 25;
+        this.dentro = 0;
+        this.jubilados = 0;
 
     }
-public void entrarSala() {
-    
-}
 
-public void entrarSalaJubilado() {
-    
-}
+    public synchronized boolean entrarSala() throws InterruptedException {
 
-public void salirSala() {
-    
-}
+        boolean rta = false;
 
-public void notificarTemperatura() {
+        while (jubilados > 0) {
+            this.wait();
+        }
 
-    
-}
+        if (dentro <= cupo) {
+
+            dentro++;
+            rta=true;
+
+        }
+
+        return rta;
+
+    }
+
+    public synchronized boolean entrarSalaJubilado() {
+
+        boolean rta=false;
+
+        jubilados++;
+
+        if(dentro>=cupo) {
+
+            rta=true;
+            
+        } 
+        return rta;
+
+    }
+
+    public void salirSala() {
+
+    }
+
+    public void notificarTemperatura() {
+
+    }
 }
